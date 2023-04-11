b52f1dcc492f955ba3e789c21df6f010c08619e3
arif <arif-celik56@hotmail.com>
2023-04-12 01:10:19 +0300

    component views

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,9 @@
 <script setup>
-/** @param {DragEvent} e  */
-async function handleDrop(e) {
-  const files = e.dataTransfer.files;
-
-  for (const file of files) {
-    window.api.uploadFile(file.path);
-  }
-}
-
-const selectFile = window.api.selectFile;
+import Dropzone from './components/Dropzone.vue';
+import Modal from './components/Modal.vue';
 </script>
 
 <template>
-  <div 
-    id="dropzone"
-    @click="selectFile()"
-    @dragover.stop.prevent=""
-    @drop.stop.prevent="handleDrop"
-  >
-    item upload
-  </div>
+  <Modal />
+  <Dropzone />
 </template>
-
-<style scoped>
-#dropzone {
-  width: 37rem;
-  height: 16rem;
-  padding: 1.5rem;
-  border: 2px dashed #d1d5db;
-  cursor: pointer;
-  border-radius: 10px;
-  background-color: #1a1a1a;
-}
-
-#dropzone:hover {
-  background-color: #2c2c2c;
-}
-</style>
